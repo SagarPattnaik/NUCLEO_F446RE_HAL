512a546a90af390e2bed5d1f6bd1d9e9359edd09
SagarPattnaik <sagarpattnaik60@gmail.com>
2024-11-29 15:39:59 -0500

    HAL project Setup

---
+++ Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.d
@@ -0,0 +1,52 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Inc/stm32f4xx_hal_conf.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+../Inc/stm32f4xx_hal_conf.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/core_cm4.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/cmsis_version.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/cmsis_compiler.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Include/mpu_armv7.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+C:/Sagar/Repo/GitHub/Demo/NUCLEO_F446RE_HAL/HAL_Project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
